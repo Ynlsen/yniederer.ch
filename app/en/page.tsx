8f493ac925141071907e060994db7185b9b19968
Ynlsen <yannick.niederer@shinternet.ch>
2025-05-19 21:28:00 +0200

    added image

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -1,8 +1,15 @@
 export default function HomePage() {
   return (
-    <main className="bg-black text-white min-h-screen flex flex-col items-center justify-center text-center">
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <div className="w-64 h-64 mb-12">
+        <img
+          src="/avatar.jpg"
+          alt="Yannick Niederer"
+          className="w-full h-full rounded-full"
+        />
+      </div>
       <div>
-        <a href="https://github.com/Ynlsen" className="hover:underline">
+        <a href="https://github.com/Ynlsen" className="text-white hover:underline">
           <h1 className="text-4xl font-bold">Work in Progress</h1>
         </a>
       </div>

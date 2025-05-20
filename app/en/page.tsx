26b3a7992a07914a7438f96dbb1757b50b059f76
Ynlsen <yannick.niederer@shinternet.ch>
2025-05-20 14:31:18 +0200

    increased colorfulness of the background

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -2,12 +2,12 @@ export default function HomePage() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
 
-      <div className="absolute -top-1/4 -left-1/4 w-1/2 h-1/2 bg-cyan/7 rounded-full blur-[100px] animate-[pulse_10s_ease_infinite]"></div>
-      <div className="absolute -bottom-1/4 -right-1/4 w-1/2 h-1/2 bg-purple/7 rounded-full blur-[100px] animate-[pulse_10s_ease_infinite]"></div>
+      <div className="absolute -top-1/4 -left-1/4 w-1/2 h-1/2 bg-cyan/10 rounded-full blur-[100px] animate-[pulse_10s_ease_infinite]"></div>
+      <div className="absolute -bottom-1/4 -right-1/4 w-1/2 h-1/2 bg-purple/10 rounded-full blur-[100px] animate-[pulse_10s_ease_infinite]"></div>
       
-      <div className="absolute w-1/2 h-1/3 bg-gradient-to-r from-cyan via-purple to-cyan opacity-5 rounded-full blur-[100px]"></div>
+      <div className="absolute w-1/2 h-1/3 bg-gradient-to-r from-cyan via-purple to-cyan opacity-10 rounded-full blur-[100px]"></div>
 
-      <div className="absolute inset-0 bg-gradient-to-br from-cyan/3 via-transparent to-purple/3"></div>
+      <div className="absolute inset-0 bg-gradient-to-br from-cyan/5 via-transparent to-purple/5"></div>
 
 
       <div className="group relative w-64 h-64 mb-12 mt-4">

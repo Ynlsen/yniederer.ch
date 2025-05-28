563020e03ce92828c6fb5b3daf1dd597d9534a13
Ynlsen <yannick.niederer@shinternet.ch>
2025-05-28 20:35:29 +0200

    discord webhook api call structure

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,3 +1,20 @@
-export async function POST(req) {
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
     const { name, email, message } = await req.json();
+
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        embeds: [
+          {
+            title: name,
+            description: message,
+            footer: { text: email }
+          },
+        ],
+      }),
+    });
 }

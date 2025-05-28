cf7c7d4c70996b2773dd19490a38535f0f25762e
Ynlsen <yannick.niederer@shinternet.ch>
2025-05-28 20:45:44 +0200

    tabs

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,21 +1,21 @@
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-    const { name, email, message } = await req.json();
+  const { name, email, message } = await req.json();
 
     const url = process.env.DISCORD_WEBHOOK_URL;
-
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        embeds: [
-          {
-            title: name,
-            description: message,
-            footer: { text: email }
-          },
-        ],
-      }),
-    });
+    
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      embeds: [
+        {
+          title: name,
+          description: message,
+          footer: { text: email }
+        },
+      ],
+    }),
+  });
 }

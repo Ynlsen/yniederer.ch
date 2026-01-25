0adb817379010e58757ba9b8cc57c317e220d3aa
Ynlsen <yannick.niederer@shinternet.ch>
2026-01-25 17:15:11 +0100

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export default function LandingLayout({
     return (
       <html suppressHydrationWarning>
         <head>
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <ThemeScript />
         </head>
         <body>
